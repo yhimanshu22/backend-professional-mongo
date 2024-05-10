b33f3190e15e96a9b3952166ed216753de1ba0f1
HIMANSHU YADAV <yhimanshu22@iitk.ac.in>
2024-05-10 15:41:44 +0530

    tested api

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,17 +4,7 @@ import { User } from '../models/user.model.js';
 import { uploadOnCloudinary } from '../utils/cloudinary.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
 
-const registerUser = asyncHandler(async (req, res) => {
-    //get user details from frontend
-    //validation
-    //check if user already exist [username,email]
-    //check for images,check for avatar
-    //upload them to cloudinary ,avatar
-    //create user object -create entry in db
-    //remove password and refresh token field from response
-    //return res
-
-    const registerUser = asyncHandler( async (req, res) => {
+const registerUser = asyncHandler( async (req, res) => {
         // get user details from frontend
         // validation - not empty
         // check if user already exists: username, email
@@ -88,6 +78,5 @@ const registerUser = asyncHandler(async (req, res) => {
     
     } )
 
-})
 
 export {registerUser}
--- src/db/index.js
+++ src/db/index.js
@@ -1,15 +1,14 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}${DB_NAME}`);
+        console.log(`MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log("MONGODB connection FAILED ", error);
-        process.exit(1)
+        console.error("MONGODB connection FAILED ", error);
+        process.exit(1);
     }
-}
+};
 
-export {connectDB}
+export { connectDB };
