42d3597c1301170303db89316a57d07c4f900a1c
HIMANSHU YADAV <yhimanshu22@iitk.ac.in>
2024-05-14 16:32:57 +0530

    chore: Update user.controller.js and user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,7 @@
 import Router from 'express';
-import  {loginUser, logoutUser, refreshAccessToken, registerUser}  from '../controllers/user.controller.js';
+import  {changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateCoverImageAvatar, updateUserAvatar}  from '../controllers/user.controller.js';
 import { upload} from '../middlewares/multer.middleware.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
-
 const router = Router();
 
 router.route('/register').post(
@@ -15,14 +14,23 @@ router.route('/register').post(
             name:'coverImage',
             maxCount:1
         }
-    ]),  
-     registerUser)
+    ]), registerUser)
 
 router.route('/login').post(loginUser)
 
 //secured routes
 router.route('/logout').post(verifyJWT,logoutUser)
 router.route('/refresh-token').post(refreshAccessToken)
+router.route('/change-password').post(verifyJWT,changeCurrentPassword)
+router.route('/current-user').get(verifyJWT,getCurrentUser);
+router.route('/update-account').patch(verifyJWT,updateAccountDetails);
+router.route('/avatar').patch(verifyJWT,upload.single('avatar'),updateUserAvatar);
+router.route('/cover-image').patch(verifyJWT,upload.single('coverImage'),updateCoverImageAvatar);
+router.route('/c/:username').get(verifyJWT,getUserChannelProfile);
+router.route('/history').get(verifyJWT,getWatchHistory)
+
+
+
 
 
 export  default router;//check this {router} is not allowed
