2560b5e116d936c294757c2c9c8a0c064c35d580
HIMANSHU YADAV <yhimanshu22@iitk.ac.in>
2024-05-14 17:14:44 +0530

    feat: Add comment, like, playlist, and tweet models

---
+++ src/models/tweet.model.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const tweetSchema = new mongoose.Schema({
+    content:{
+        type:String,
+        required:true
+    },
+    owner:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }
+},{timestamps:true})
+
+export const Tweet = mongoose.model("Tweet",tweetSchema)
